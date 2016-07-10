2a658ea6dca8dd76ec9d910003f6160a92b7e236
skamkar <sjk@zestfinance.com>
2016-07-09 20:25:19 -0700

    Create tribe

---
+++ lib/tribe.rb
@@ -0,0 +1,22 @@
+class Tribe
+
+  attr_reader :name, :members
+
+  def initialize(options = {})
+    @name = options[:name]
+    @members = options[:members]
+    puts "The #{@name} tribe has been created."
+  end
+
+  def to_s
+    @name
+  end
+
+  def tribal_council(immune)
+    if immune.keys[0] == :immune
+      Contestant.new(nil)
+    else
+      immune.values[0]
+    end
+  end
+end
