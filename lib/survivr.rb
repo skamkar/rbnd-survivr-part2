33c123fda2b17c91a286112d11d5d2a5aa516008
skamkar <sjk@zestfinance.com>
2016-07-12 21:44:26 -0700

    Handle phase nesting to pass tests and allow for proper code exeuction

--- lib/survivr.rb
+++ lib/survivr.rb
@@ -6,18 +6,17 @@ require 'byebug'
 
 #After your tests pass, uncomment this code below
 #=========================================================
-# # Create an array of twenty hopefuls to compete on the island of Borneo
-# @contestants = %w(carlos walter aparna trinh diego juliana poornima juha sofia julia fernando dena orit colt zhalisa farrin muhammed ari rasha gauri)
-# @contestants.map!{ |contestant| Contestant.new(contestant) }.shuffle!
-#
-# # Create two new tribes with names
-# @coyopa = Tribe.new(name: "Pagong", members: @contestants.shift(10))
-# @hunapu = Tribe.new(name: "Tagi", members: @contestants.shift(10))
-#
-# # Create a new game of Survivor
-# @borneo = Game.new(@coyopa, @hunapu)
-#=========================================================
+# Create an array of twenty hopefuls to compete on the island of Borneo
+@contestants = %w(carlos walter aparna trinh diego juliana poornima juha sofia julia fernando dena orit colt zhalisa farrin muhammed ari rasha gauri)
+@contestants.map!{ |contestant| Contestant.new(contestant) }.shuffle!
 
+# Create two new tribes with names
+@coyopa = Tribe.new(name: "Pagong", members: @contestants.shift(10))
+@hunapu = Tribe.new(name: "Tagi", members: @contestants.shift(10))
+
+# Create a new game of Survivor
+@borneo = Game.new(@coyopa, @hunapu)
+#=========================================================
 
 #This is where you will write your code for the three phases
 def phase_one
@@ -42,14 +41,17 @@ def phase_one
     # Print result to terminal
     puts "#{losing_team} lost and have eliminated #{eliminated.last}."
   end
+
   eliminated.length
 end
 
 def phase_two
-  # Phase 1 prep (conduct Phase 1, merge into a single tribe)
-  phase_one
-  @merged_tribe = @borneo.merge("Cello")
-  @merged_game = Game.new(@merged_tribe)
+  # Perform phase_one if necessary
+  unless @merge_tribe.members.length == 12
+    phase_one
+    @merge_tribe = @borneo.merge("Cello")
+  end
+  @merge_game = Game.new(@merge_tribe)
 
   puts "\nPHASE TWO"    
   eliminated = []
@@ -57,40 +59,47 @@ def phase_two
 
   3.times do
     # Eliminate random contestant for each challenge
-    eliminated << @merged_game.individual_immunity_challenge
-    @merged_tribe.members.delete(eliminated.last)
-    puts "From #{@merged_tribe.name}, #{eliminated.last} has been eliminated."
+    eliminated << @merge_game.individual_immunity_challenge
+    @merge_tribe.members.delete(eliminated.last)
+    puts "From #{@merge_tribe.name}, #{eliminated.last} has been eliminated."
     # Assign winner contestant (with immunity) for each challenge
-    immune << @merged_game.individual_immunity_challenge
+    immune << @merge_game.individual_immunity_challenge
   end
 
   eliminated.length  
 end
 
 def phase_three
-  # Phase 1 & 2 prep
-  phase_two
+  # Perform phase_two (and phase_one) if necessary
+  unless @merge_tribe.members.length == 9
+    phase_two
+  end
 
   puts "\nPHASE THREE"
   jury = []
   7.times do
-    jury << @merged_game.individual_immunity_challenge
-    @merged_tribe.members.delete(jury.last)
+    jury << @merge_game.individual_immunity_challenge
+    @merge_tribe.members.delete(jury.last)
     puts "#{jury.last} has been eliminated and assigned to the Jury."
   end
   @jury.members = jury
   jury.length
 end
 
-
 # If all the tests pass, the code below should run the entire simulation!!
 #=========================================================
-# phase_one #8 eliminations
-# @merge_tribe = @borneo.merge("Cello") # After 8 eliminations, merge the two tribes together
-# phase_two #3 more eliminations
-# @jury = Jury.new
-# phase_three #7 elminiations become jury members
-# finalists = @merge_tribe.members #set finalists
-# vote_results = @jury.cast_votes(finalists) #Jury members report votes
-# @jury.report_votes(vote_results) #Jury announces their votes
-# @jury.announce_winner(vote_results) #Jury announces final winner
+phase_one #8 eliminations
+@merge_tribe = @borneo.merge("Cello") # After 8 eliminations, merge the two tribes together
+phase_two #3 more eliminations
+@jury = Jury.new
+phase_three #7 elminiations become jury members
+finalists = @merge_tribe.members #set finalists
+
+puts "\nJURY VOTES:"
+vote_results = @jury.cast_votes(finalists) #Jury members report votes
+
+puts "\nJURY TALLY:"
+@jury.report_votes(vote_results) #Jury announces their votes
+
+puts "\nFINAL RESULT:"
+@jury.announce_winner(vote_results) #Jury announces final winner
